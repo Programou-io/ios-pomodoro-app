a9bf56154af5b43386b34d112337e50c0ccd922d
Paolo Prodossimo Lopes <paolo.prodossimo.lopes@gmail.com>
2023-09-03 11:08:45 -0300

    chore: initial project setup

---
+++ Pom/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  Pom
+//
+//  Created by Paolo Prodossimo Lopes on 03/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
