4be65461ae9cf600aa2bfb05521fc1a732345827
Paolo Prodossimo Lopes <paolo.prodossimo.lopes@gmail.com>
2023-09-03 13:16:57 -0300

    choore: testing format on pre-commit hook

--- Pom/ViewController.swift
+++ Pom/ViewController.swift
@@ -1,12 +1,9 @@
 import UIKit
 
-class ViewController: UIViewController {
+class   ViewController: UIViewController {
 
-    override func viewDidLoad() {
+        override func viewDidLoad() {
         super.viewDidLoad()
-    }
-
-    
-    
-    
+        }
+        
 }
