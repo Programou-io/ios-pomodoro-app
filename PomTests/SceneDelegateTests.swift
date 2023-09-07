ada2934bdab6ea4fa1fb29efe61692a968262133
Paolo Prodossimo Lopes <paolo.prodossimo.lopes@gmail.com>
2023-09-06 21:18:11 -0300

    test: ensure the view controller is setuped in scene delegate

---
+++ PomTests/SceneDelegateTests.swift
@@ -0,0 +1,43 @@
+import XCTest
+
+@testable import Pom
+
+final class SceneDelegateTests: XCTestCase {
+
+  func test_setupWindow_shouldMakeKeyAndVisibleOnce() {
+    let window = WindowSpy()
+    let sut = SceneDelegate()
+
+    sut.setupWindow(window: window)
+
+    XCTAssertEqual(window.makeKeyAndVisibleCallCount, 1)
+  }
+
+  func test_setupWindow_shouldSetRootViewControlelrAsViewController() {
+    let window = WindowSpy()
+    let sut = SceneDelegate()
+
+    sut.setupWindow(window: window)
+
+    XCTAssertTrue(window.rootViewController is ViewController)
+  }
+
+  func test_setupWindow_shouldReplaceInstanceWindowToTheProvided() {
+    let window = WindowSpy()
+    let sut = SceneDelegate()
+
+    sut.setupWindow(window: window)
+
+    XCTAssertIdentical(sut.window, window)
+  }
+}
+
+extension SceneDelegateTests {
+  fileprivate final class WindowSpy: UIWindow {
+    private(set) var makeKeyAndVisibleCallCount = 0
+
+    override func makeKeyAndVisible() {
+      makeKeyAndVisibleCallCount += 1
+    }
+  }
+}
