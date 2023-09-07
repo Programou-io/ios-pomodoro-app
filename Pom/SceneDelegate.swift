32e337db7814eb1318eb46ad1242ada1f7da7f16
Paolo Prodossimo Lopes <paolo.prodossimo.lopes@gmail.com>
2023-09-06 21:19:27 -0300

    style: change tab width to  8 instead of 4

--- Pom/SceneDelegate.swift
+++ Pom/SceneDelegate.swift
@@ -2,18 +2,18 @@ import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
-  var window: UIWindow?
+    var window: UIWindow?
 
-  func scene(
-    _ scene: UIScene, willConnectTo session: UISceneSession,
-    options connectionOptions: UIScene.ConnectionOptions
-  ) {
-    setupWindow(window: UIWindow(frame: UIScreen.main.bounds))
-  }
+    func scene(
+        _ scene: UIScene, willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions
+    ) {
+        setupWindow(window: UIWindow(frame: UIScreen.main.bounds))
+    }
 
-  func setupWindow(window: UIWindow) {
-    self.window = window
-    window.rootViewController = ViewController()
-    window.makeKeyAndVisible()
-  }
+    func setupWindow(window: UIWindow) {
+        self.window = window
+        window.rootViewController = ViewController()
+        window.makeKeyAndVisible()
+    }
 }
--- PomTests/SceneDelegateTests.swift
+++ PomTests/SceneDelegateTests.swift
@@ -4,40 +4,40 @@ import XCTest
 
 final class SceneDelegateTests: XCTestCase {
 
-  func test_setupWindow_shouldMakeKeyAndVisibleOnce() {
-    let window = WindowSpy()
-    let sut = SceneDelegate()
+    func test_setupWindow_shouldMakeKeyAndVisibleOnce() {
+        let window = WindowSpy()
+        let sut = SceneDelegate()
 
-    sut.setupWindow(window: window)
+        sut.setupWindow(window: window)
 
-    XCTAssertEqual(window.makeKeyAndVisibleCallCount, 1)
-  }
+        XCTAssertEqual(window.makeKeyAndVisibleCallCount, 1)
+    }
 
-  func test_setupWindow_shouldSetRootViewControlelrAsViewController() {
-    let window = WindowSpy()
-    let sut = SceneDelegate()
+    func test_setupWindow_shouldSetRootViewControlelrAsViewController() {
+        let window = WindowSpy()
+        let sut = SceneDelegate()
 
-    sut.setupWindow(window: window)
+        sut.setupWindow(window: window)
 
-    XCTAssertTrue(window.rootViewController is ViewController)
-  }
+        XCTAssertTrue(window.rootViewController is ViewController)
+    }
 
-  func test_setupWindow_shouldReplaceInstanceWindowToTheProvided() {
-    let window = WindowSpy()
-    let sut = SceneDelegate()
+    func test_setupWindow_shouldReplaceInstanceWindowToTheProvided() {
+        let window = WindowSpy()
+        let sut = SceneDelegate()
 
-    sut.setupWindow(window: window)
+        sut.setupWindow(window: window)
 
-    XCTAssertIdentical(sut.window, window)
-  }
+        XCTAssertIdentical(sut.window, window)
+    }
 }
 
 extension SceneDelegateTests {
-  fileprivate final class WindowSpy: UIWindow {
-    private(set) var makeKeyAndVisibleCallCount = 0
+    fileprivate final class WindowSpy: UIWindow {
+        private(set) var makeKeyAndVisibleCallCount = 0
 
-    override func makeKeyAndVisible() {
-      makeKeyAndVisibleCallCount += 1
+        override func makeKeyAndVisible() {
+            makeKeyAndVisibleCallCount += 1
+        }
     }
-  }
 }
